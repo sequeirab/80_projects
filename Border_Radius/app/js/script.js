a4950eb41cc852ce92987cb5774f27950528221f
Bruno Sequeira <sequeirab86@gmail.com>
2021-03-24 13:35:05 +0000

    done with changing border radius

--- Border_Radius/app/js/script.js
+++ Border_Radius/app/js/script.js
@@ -1,1 +1,31 @@
-console.log('Teste')
+const inputTopLeft = document.getElementById('input-top-left');
+const inputTopRight = document.getElementById('input-top-right');
+const inputBottomLeft = document.getElementById('input-bottom-left');
+const inputBottomRight = document.getElementById('input-bottom-right');
+const borderBox = document.getElementById('border-box')
+const output = document.getElementById('output');
+const inputs = document.querySelectorAll('input');
+
+inputTopLeft.addEventListener('input',  () => {
+    borderBox.style.borderTopLeftRadius = inputTopLeft.value + 'px';
+     
+})
+
+inputTopRight.addEventListener('input', () => {
+    borderBox.style.borderTopRightRadius = inputTopRight.value + 'px';
+})
+
+
+inputBottomLeft.addEventListener('input', () => {
+    borderBox.style.borderBottomLeftRadius = inputBottomLeft.value + 'px';
+})
+
+inputBottomRight.addEventListener('input', () => {
+    borderBox.style.borderBottomRightRadius = inputBottomRight.value + 'px';
+})
+
+for(let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', () => {
+
+    })
+}
